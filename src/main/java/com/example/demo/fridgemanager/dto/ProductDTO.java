26d0994291a98c67d875bdd2c813f549de5b00ba
Kamil Skomro <kamilskomrokamil@gmail.com>
2021-05-24 18:48:19 +0200

    add draft for fitnesse test

--- src/main/java/com/example/demo/fridgemanager/dto/ProductDTO.java
+++ src/main/java/com/example/demo/fridgemanager/dto/ProductDTO.java
@@ -1,6 +1,10 @@
 package com.example.demo.fridgemanager.dto;
 
 import com.example.demo.fridgemanager.entities.Product;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import java.time.LocalDate;
 
@@ -8,6 +12,9 @@ public class ProductDTO {
     private Long id;
     private String name;
     private Integer kcal;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate expiryDate;
     private Integer quantity;
     private Integer minQuantity;
