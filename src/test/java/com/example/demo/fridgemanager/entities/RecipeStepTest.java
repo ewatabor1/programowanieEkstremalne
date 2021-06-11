fc40949b7506676d7c389ef1d66101ee56172d3a
Tomasz Krawczyk <kravcu2@gmail.com>
2021-06-11 18:24:29 +0200

    Added unit tests for RecipeStep

---
+++ src/test/java/com/example/demo/fridgemanager/entities/RecipeStepTest.java
@@ -0,0 +1,50 @@
+package com.example.demo.fridgemanager.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeStepTest {
+    private RecipeStep recipeStep;
+    private Recipe recipe;
+    private final String recipeInstruction = "This is instruction for the recipe";
+
+    @BeforeEach
+    void setUp() {
+        Product pepsi = new Product("pepsi", 250, LocalDate.now(), null, null, null, null, null);
+        recipe = new Recipe.Builder()
+                .setName("student's breakfast")
+                .setDescription("typical breakfast")
+                .addIngredient(pepsi, BigDecimal.valueOf(2))
+                .addInstruction("drink pepsi")
+                .create();
+        recipeStep = new RecipeStep(recipe, recipeInstruction);
+    }
+
+    @Test
+    void getRecipe() {
+        assertEquals(recipeStep.getRecipe(), recipe);
+    }
+
+    @Test
+    void getInstruction() {
+        assertEquals(recipeStep.getInstruction(), recipeInstruction);
+    }
+
+    @Test
+    void setRecipe() {
+        Product cola = new Product("cola", 250, LocalDate.now(), null, null, null, null, null);
+        Recipe newRecipe = new Recipe.Builder()
+                .setName("students drink")
+                .setDescription("typical drink")
+                .addIngredient(cola, BigDecimal.valueOf(1))
+                .addInstruction("drink cola")
+                .create();
+        recipeStep.setRecipe(newRecipe);
+        assertEquals(recipeStep.getRecipe(), newRecipe);
+    }
+}
