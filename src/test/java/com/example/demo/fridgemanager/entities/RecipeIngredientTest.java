94f1d0e77f36455e0e3317e364f9af2462958599
Tomasz Krawczyk <kravcu2@gmail.com>
2021-06-11 18:31:21 +0200

    Added unit tests for RecipeIngredient

---
+++ src/test/java/com/example/demo/fridgemanager/entities/RecipeIngredientTest.java
@@ -0,0 +1,78 @@
+package com.example.demo.fridgemanager.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeIngredientTest {
+    private Product product;
+    private Recipe recipe;
+    private BigDecimal amount;
+    private RecipeIngredient recipeIngredient;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product("pepsi", 250, LocalDate.now(), null, null, null, null, null);
+        recipe = new Recipe.Builder()
+                .setName("student's breakfast")
+                .setDescription("typical breakfast")
+                .addIngredient(product, BigDecimal.valueOf(2))
+                .addInstruction("drink pepsi")
+                .create();
+        amount = BigDecimal.valueOf(123);
+        recipeIngredient = new RecipeIngredient(recipe, product, amount);
+
+
+    }
+
+    @Test
+    void getRecipe() {
+        assertEquals(recipeIngredient.getRecipe(), recipe);
+    }
+
+    @Test
+    void setRecipe() {
+        Product cola = new Product("cola", 250, LocalDate.now(), null, null, null, null, null);
+        Recipe newRecipe = new Recipe.Builder()
+                .setName("students drink")
+                .setDescription("typical drink")
+                .addIngredient(cola, BigDecimal.valueOf(1))
+                .addInstruction("drink cola")
+                .create();
+        recipeIngredient.setRecipe(newRecipe);
+        assertEquals(recipeIngredient.getRecipe(), newRecipe);
+    }
+
+    @Test
+    void getProduct() {
+        assertEquals(recipeIngredient.getProduct(), product);
+    }
+
+    @Test
+    void setProduct() {
+        Product cola = new Product("cola", 250, LocalDate.now(), null, null, null, null, null);
+        recipeIngredient.setProduct(cola);
+        assertEquals(recipeIngredient.getProduct(), cola);
+    }
+
+    @Test
+    void getAmount() {
+        assertEquals(recipeIngredient.getAmount(), amount);
+    }
+
+    @Test
+    void setAmount() {
+        BigDecimal newAmount = new BigDecimal(456);
+        recipeIngredient.setAmount(newAmount);
+        assertEquals(recipeIngredient.getAmount(), newAmount);
+    }
+
+    @Test
+    void getId() {
+        assertNull(recipeIngredient.getId());
+    }
+}
