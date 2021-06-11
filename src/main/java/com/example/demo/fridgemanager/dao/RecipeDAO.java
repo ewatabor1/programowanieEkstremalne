48c948aaae186fc8d989cbba8b8093e36377cea2
Tomasz Kolbusz <tomaszkolbusz1@gmail.com>
2021-06-11 07:11:30 +0200

    change select for Recipes

--- src/main/java/com/example/demo/fridgemanager/dao/RecipeDAO.java
+++ src/main/java/com/example/demo/fridgemanager/dao/RecipeDAO.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class RecipeDAO extends DAO<Recipe> {
@@ -15,24 +20,39 @@ public class RecipeDAO extends DAO<Recipe> {
     private EntityManager entityManager;
 
     public List<Recipe> findAll() {
-        return entityManager.createQuery("SELECT r FROM Recipe r LEFT JOIN RecipeIngredient  ri ON ri.recipe.id = r.id LEFT JOIN RecipeStep rs ON rs.recipe.id = r.id").getResultList();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Recipe> query = builder.createQuery(Recipe.class);
+        Root<Recipe> root = query.from(Recipe.class);
+        TypedQuery<Recipe> allQuery = entityManager.createQuery(query.select(root));
+        return allQuery.getResultList();
     }
 
     public Recipe findById(Long id) {
         try {
-            Object result = entityManager.createQuery("SELECT r FROM Recipe r LEFT JOIN RecipeIngredient  ri ON ri.recipe.id = r.id LEFT JOIN RecipeStep rs ON rs.recipe.id = r.id WHERE r.id = :id")
-                    .setParameter("id", id)
-                    .getSingleResult();
-            return ((Recipe) result);
+            CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Recipe> query = builder.createQuery(Recipe.class);
+            Root<Recipe> root = query.from(Recipe.class);
+            TypedQuery<Recipe> allQuery = entityManager.createQuery(
+                    query.select(root)
+                            .where(builder.equal(root.get("id"), id))
+            );
+            return allQuery.getSingleResult();
         } catch (NoResultException noResult) {
             return null;
         }
     }
+
     public List<Recipe> findByName(String name) {
-            return entityManager.createQuery("SELECT r FROM Recipe r LEFT JOIN RecipeIngredient  ri ON ri.recipe.id = r.id LEFT JOIN RecipeStep rs ON rs.recipe.id = r.id WHERE UPPER(r.name) = UPPER(:name)")
-                    .setParameter("name", name)
-                    .getResultList();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Recipe> query = builder.createQuery(Recipe.class);
+        Root<Recipe> root = query.from(Recipe.class);
+        TypedQuery<Recipe> allQuery = entityManager.createQuery(
+                query.select(root)
+                        .where(builder.equal(builder.upper(root.get("name")), name.toUpperCase(Locale.ROOT)))
+        );
+        return allQuery.getResultList();
     }
+
     @Transactional
     public void delete(Long id) {
         entityManager.createQuery("DELETE FROM RecipeStep  WHERE recipe.id = :id")
